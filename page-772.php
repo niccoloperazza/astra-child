7d2152b1bd7f88d03e877f4e547ca353c9dfebd4
Nico <niccolo.perazza@gmail.com>
2025-07-09 11:46:54 +0200

    Aggiunta delle recensioni alla landing page

--- page-772.php
+++ page-772.php
@@ -54,4 +54,57 @@ $immagine_hero = get_field('immagine_hero');
 	</div>
 </div>
 
+<div class="recensioni-wrapper">
+	<h3>Recensioni</h3>
+	<div class="recensioni">
+		<div class="recensione">
+			<h4>Recensione 1</h4>
+			<div class="stelle">★★★★☆</div>
+			<div class="testo-recensione">Ottimo servizio</div>
+			<p class="nome">Pinco Palla</p>
+		</div>
+		<div class="recensione">
+			<h4>Recensione 2</h4>
+			<div class="stelle" data-stars="4" ></div>
+			<div class="testo-recensione">Ottimo servizio</div>
+			<p class="nome">Pinco Palla</p>
+		</div>
+		<div class="recensione">
+			<h4>Recensione 3</h4>
+			<div class="stelle" data-stars="3" ></div>
+			<div class="testo-recensione">Ottimo servizio</div>
+			<p class="nome">Pinco Palla</p>
+		</div>
+	</div>
+</div>
+
+<script>
+	document.addEventListener('DOMContentLoaded', function() {
+		let ratingElements = document.querySelectorAll('[data-stars]');
+
+		//console.log(ratingElements);
+
+		ratingElements.forEach(function(element) {
+
+			const stelleMax = 5;
+			let stelle = element.dataset.stars;
+			let stelleVuote = stelleMax - stelle;
+			// console.log(stelle);
+
+			let stelleHTML = '';
+
+			for (let i = 1; i <= stelle; i++) {
+				stelleHTML += '★';
+			}
+
+			for (let i = 1; i <= stelleVuote; i++) {
+				stelleHTML += '☆';
+			}
+
+			element.innerHTML = stelleHTML;
+		});
+
+	});
+</script>
+
 <?php get_footer(); ?>
